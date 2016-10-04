c86715444042f549eed481f99b94cc73f95ddd05
vankai <625608852@qq.com>
2016-10-04 17:27:28 +0800

    first_try

---
+++ index.js
@@ -0,0 +1,18 @@
+var datespan = document.getElementById("bg-date"),
+    monthspan = document.getElementById("bg-month"),
+    today = new Date(),
+    month = today.getMonth(),
+    date = today.getDate(),
+    monthNames = ['January', 'February', 'March',
+        'April', 'May', 'June',
+        'July', 'August', 'September',
+        'October', 'November', 'December'
+    ];
+
+function prefix(n) {
+    return (n < 10) ? ("0" + n) : n.toString();
+}
+
+
+datespan.textContent = prefix(date);
+monthspan.textContent = monthNames[month];
