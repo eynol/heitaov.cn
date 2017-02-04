a8ba2077740a7657f2af3334c2b57c9c8f6c2f22
vankai <625608852@qq.com>
2017-02-04 17:12:12 +0800

    change email & practice gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,36 @@
-var gulp = require('gulp');
-var del = require('del');
+const gulp = require('gulp');
+const del = require('del');
 const imagemin = require('gulp-imagemin');
+const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
   console.log("default task")
 });
 
-gulp.task('clean:build',function(){
-  del(["build"]).then(()=>{
+gulp.task('watch',['watch:index'], function () {})
+
+gulp.task('watch:index', function () {
+  return gulp
+    .watch("index.styl", function () {
+      gulp
+        .src('index.styl')
+        .pipe(sourcemaps.init())
+        .pipe(stylus({compress: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./'))
+    })
+
+})
+
+gulp.task('clean:build', function () {
+  return del(["build"]).then(() => {
     console.log("finished clean build")
   })
 })
 
 gulp.task('imagemin', function () {
-  gulp
+  return gulp
     .src('img/**')
     .pipe(imagemin())
     .pipe(gulp.dest('imgmin'))
