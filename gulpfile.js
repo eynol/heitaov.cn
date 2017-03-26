13646555c31bc1c7dbf73fba1664e3905b983898
vankai <625608852@qq.com>
2017-03-26 17:24:09 +0800

    change style

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,46 @@ const del = require('del');
 const imagemin = require('gulp-imagemin');
 const stylus = require('gulp-stylus');
 const sourcemaps = require('gulp-sourcemaps');
+var pug = require('gulp-pug');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-gulp.task('default', function () {
-  console.log("default task")
-});
-
-gulp.task('watch',['watch:index'], function () {})
-
-gulp.task('watch:index', function () {
+gulp.task('views:index', function buildHTML() {
   return gulp
-    .watch("index.styl", function () {
-      gulp
-        .src('index.styl')
-        .pipe(sourcemaps.init())
-        .pipe(stylus({compress: true}))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./'))
-    })
+    .src('index.pug')
+    .pipe(plumber())
+    .pipe(pug())
+    .pipe(gulp.dest('./build'))
+});
+gulp.task('stylus:index', function () {
+  return gulp
+    .src('index.styl')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(stylus({compress: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./build'))
+    .pipe(reload({stream: true}))
 
 })
+gulp.task('default', ['views:index', 'stylus:index']);
+
+gulp.task('watch', ['watch:index'], function () {})
+
+gulp.task('watch:index', function () {
+  gulp.watch("index.styl", ['stylus:index'])
+  gulp.watch("index.pug", ['views:index'])
+})
+
+gulp.task('serve', ['watch'], function () {
+
+  browserSync.init({server: "./build",port:7777});
+
+  gulp
+    .watch("build/*.html")
+    .on('change', reload);
+});
 
 gulp.task('clean:build', function () {
   return del(["build"]).then(() => {
@@ -30,6 +51,7 @@ gulp.task('clean:build', function () {
 })
 
 gulp.task('imagemin', function () {
+
   return gulp
     .src('img/**')
     .pipe(imagemin())
